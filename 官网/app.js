ae5752f4f8b050d2d4e8a41ca706ff580673e0aa
cn6696826 <544868267@qq.com>
2018-01-06 17:16:36 +0800

    'ss'

--- 官网/app.js
+++ 官网/app.js
@@ -23,6 +23,16 @@ app.get(/abdd/, function(req, res) {
 
 })
 
+// app.route()路由路径的链式句柄
+app.route('/ad2').get(function(res, req) {
+    res.send('222')
+}).post(function(res, req) {
+    res.send('2223')
+})
+
+var router = express.Router();
+
+
 var server = app.listen(3000, function() {
     //node api ==server.address 找到指定的端口 返回三个值 port （端口） address(地址) family (IPV4协议)
     //只有在listen事件的时候才会触发    
